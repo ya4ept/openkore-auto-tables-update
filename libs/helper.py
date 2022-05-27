e027cf4de17ae1d4f82dfc7704610846d371a305
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2022-05-27 06:47:46 +0000

    fix error

--- libs/helper.py
+++ libs/helper.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 
+
 class Helper:
 
     def read_json(self, file):
@@ -15,9 +16,9 @@ class Helper:
 
     def convert_iteminfo_lub_txt(self) -> bool:
         cmd = f"sudo ./tools/lua32 ./tools/LUB_TO_TABLES/PARSE_ITEMINFO.lua ./patches/{self.file['server_name']}/extracted ./tables/{self.file['server_name']}"
-	process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
 
-	lines, error = process.communicate()
-	process.wait()
+        lines, error = process.communicate()
+        process.wait()
 
-	return True
+        return True
