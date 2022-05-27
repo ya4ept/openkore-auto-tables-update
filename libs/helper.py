cc4ce556417035b248d73005263b6a115489e1c7
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2022-05-27 13:43:41 +0700

    fix error

--- libs/helper.py
+++ libs/helper.py
@@ -15,11 +15,9 @@ class Helper:
 
     def convert_iteminfo_lub_txt(self) -> bool:
         cmd = f"sudo ./tools/lua32 ./tools/LUB_TO_TABLES/PARSE_ITEMINFO.lua ./patches/{self.file['server_name']}/extracted ./tables/{self.file['server_name']}"
-        print(cmd)
-		process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+	process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
 
-        lines, error = process.communicate()
-        process.wait()
+	lines, error = process.communicate()
+	process.wait()
 
-        return True
-
+	return True
